6b026e86c213e6b72d26144b57f457362f0ebf33
Yehor Kashperskyi <yehor.kashperskyi@viveapp.com>
2026-03-14 11:55:22 +0200

    feat(calculator): Implement proto deposit calculator

--- src/Features/calculator/calculator.page.ts
+++ src/Features/calculator/calculator.page.ts
@@ -1,17 +1,55 @@
-import { Component } from "@angular/core";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
+
+interface CalculatedDeposit {
+  readonly gross: number;
+}
 
 @Component({
   selector: 'page-calculator',
   templateUrl: 'calculator.page.html',
 
-  imports: [ MatFormFieldModule, MatInputModule ]
+  imports: [ MatFormFieldModule, MatInputModule, ReactiveFormsModule ]
 })
 export class CalculatorPage {
-  public constructor() {
+  public readonly calculatorForm: FormGroup;
 
+  public readonly ready = signal(false);
+
+  public readonly result = signal<CalculatedDeposit>({
+    gross: 0
+  });
+
+  public constructor(private _fb: FormBuilder) {
+    const CAPITALIZATION = 12;
+
+    this.calculatorForm = this._fb.group({
+      initialDeposit: this._fb.control(0, Validators.min(0)),
+      monthlyDeposit: this._fb.control(0, Validators.min(0)),
+      annualInterest: this._fb.control(0, Validators.min(0)),
+      monthsDuration: this._fb.control(0, Validators.min(0)),
+    });
+
+    this.calculatorForm.valueChanges.subscribe((form) => {
+      const { initialDeposit: INDE, annualInterest: ANIN, monthsDuration: MODU } = form;
+
+      if (ANIN <= 0 || INDE <= 0 || MODU <= 0) {
+        this.ready.set(false);
+        return;
+      }
+
+      const gross = INDE * (1 + ANIN / CAPITALIZATION) ** MODU
+
+      this.result.update(() => {
+        return { gross };
+      });
+
+      this.ready.set(true);
+
+    });
   }
 }
