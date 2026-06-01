b1ad6ae45363a7a9e33c75d16d2c3e9bb2ccb6ed
Yehor Kashperskyi <ekashpersky@gmail.com>
2026-06-01 03:16:50 +0300

    fix(p11n): Broken english translation

--- src/Shared/duration.pipe.ts
+++ src/Shared/duration.pipe.ts
@@ -1,4 +1,5 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 
 
@@ -8,7 +9,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false,
 })
 export class DurationPipe implements PipeTransform {
-  private _pluralizer = new Intl.PluralRules();
+  private _pluralizer: Intl.PluralRules;
+
+  public constructor() {
+    this._pluralizer = new Intl.PluralRules(
+      inject(TranslateService).getCurrentLang()
+    );
+  }
 
   public transform(duration: number, scale: string): string {
     const plural = this._pluralizer.select(duration);
